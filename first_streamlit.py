c322e71286848657645f1832f8f9572143aaf8b8
Golder <marc.golder@hs-heilbronn.de>
2026-03-09 11:17:22 +0100

    draft for first page with colocation data

--- first_streamlit.py
+++ first_streamlit.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import os
 import pandas as pd
+import plotly.graph_objects as go
+
+from plotly.subplots import make_subplots
 from dotenv import load_dotenv
 from ual.data_processor import DataProcessor
 from ual.get_config import get_config
@@ -19,29 +22,45 @@ def main():
     connection: InfluxDBConnector = InfluxDBConnector(os.getenv("INFLUX_URL"), os.getenv("INFLUX_TOKEN"),
                                                       os.getenv("INFLUX_ORG"))
 
-    inputs_query: str = InfluxQueryBuilder() \
+    ual3_query: str = InfluxQueryBuilder() \
         .set_bucket(ual_source.get_bucket()) \
         .set_range(run_config["start_time"], run_config["stop_time"]) \
         .set_topic(ual_source.get_sensor()) \
-        .set_fields(run_config["inputs"]) \
+        .set_fields(run_config["ual3_fields"]) \
         .build()
-    input_data: pd.DataFrame = connection.query_dataframe(inputs_query)
+    ual3_data: pd.DataFrame = connection.query_dataframe(ual3_query)
 
-    target_query: str = InfluxQueryBuilder() \
+    lubw_query: str = InfluxQueryBuilder() \
         .set_bucket(lubw_source.get_bucket()) \
         .set_range(run_config["start_time"], run_config["stop_time"]) \
         .set_topic(lubw_source.get_sensor()) \
-        .set_fields(run_config["targets"]) \
+        .set_fields(run_config["lubw_fields"]) \
         .build()
-    target_data: pd.DataFrame = connection.query_dataframe(target_query)
+    lubw_data: pd.DataFrame = connection.query_dataframe(lubw_query)
 
-    data_processor: DataProcessor = (DataProcessor(input_data, target_data)
+    data_processor: DataProcessor = (DataProcessor(ual3_data, lubw_data)
                                      .to_hourly()
                                      .remove_nan()
-                                     .calculate_w_a_difference(['NO', 'NO2', 'O3'])
                                      .align_dataframes_by_time())
-    st.dataframe(input_data)
-    st.line_chart(input_data['sht_humid'])
+
+    ual3_data, lubw_data = data_processor.get_inputs(), data_processor.get_targets()
+
+    "# Kollokationsmessungen an der LUBW Station in der Hans-Riesser-Straße" # TODO: Mit Inhalt füllen
+    "Hintergrundinformationen bereitstellen, die LUBW betreibt eine Messstelle ... "
+    "Seit Juli 2025 betreiben wir dort parallel eine Messstation"
+
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig.add_trace(go.Scatter(x=ual3_data.index, y=ual3_data['NO2'], mode='lines', name='UAL3 NO2'),
+                  secondary_y=False,
+                  )
+    fig.add_trace(go.Scatter(x=lubw_data.index, y=lubw_data['NO2'], mode='lines', name='LUBW NO2'),
+                  secondary_y=False,
+                  )
+    fig.add_trace(go.Scatter(x=lubw_data.index, y=lubw_data['RLF'], mode='lines', name='rel. hum'),
+                  secondary_y=True,
+                  )
+    fig.update_layout(title='Vergleich der Messergebnisse')
+    st.plotly_chart(fig)
 
 if __name__ == "__main__":
     main()
